8c63d3c83b9c16317d44d0fc71864acdfcf39542
hatrari <ham.atrari@gmail.com>
2020-05-22 21:21:50 +0200

    add post method

--- server.go
+++ server.go
@@ -2,6 +2,8 @@ package main
 
 import "net/http"
 import "encoding/json"
+import "sync"
+import "io/ioutil"
 
 type User struct {
 	Id		string
@@ -9,17 +11,35 @@ type User struct {
 }
 
 type usersHandler struct {
+	sync.Mutex
 	store map[string]User
+}
+
+func (h *usersHandler) methods(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+		case "GET":
+			h.get(w, r)
+			return
+		case "POST":
+			h.post(w, r)
+			return
+		default:
+			w.WriteHeader(http.StatusMethodNotAllowed)
+			w.Write([]byte("Method not Allowed."))
+			return
+	}
 }
 
 func (h *usersHandler) get(w http.ResponseWriter, r *http.Request) {
 	users := make([]User, len(h.store))
 
+	h.Lock()
 	i := 0
 	for _, user := range h.store {
 		users[i] = user
 		i++
 	}
+	h.Unlock()
 
 	jsonBytes, err := json.Marshal(users)
 	if err != nil {
@@ -32,6 +52,26 @@ func (h *usersHandler) get(w http.ResponseWriter, r *http.Request) {
 	w.Write(jsonBytes)
 }
 
+func (h *usersHandler) post(w http.ResponseWriter, r *http.Request) {
+	bodyBytes, err := ioutil.ReadAll(r.Body)
+	defer r.Body.Close()
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+	}
+
+	var user User
+	err = json.Unmarshal(bodyBytes, &user)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte(err.Error()))
+	}
+
+	h.Lock()
+	h.store[user.Id] = user
+	defer h.Unlock()
+}
+
 func newUsersHandler() *usersHandler {
 	return &usersHandler {
 		store: map[string]User{
@@ -42,7 +82,7 @@ func newUsersHandler() *usersHandler {
 
 func main() {
 	usersHandler := newUsersHandler()
-	http.HandleFunc("/users", usersHandler.get)
+	http.HandleFunc("/users", usersHandler.methods)
 	err := http.ListenAndServe(":8000", nil)
 	if err != nil {
 		panic(err)
